1d4c3b45e4e673b11651decea7041c4e35bd8caa
Dan <dbulman68@gmail.com>
2022-09-28 22:25:00 +0100

    Fully implemented Double Click and Timer modifications and made everything work as its supposed to

--- AutoClicker Interface/MainWindow.xaml.cs
+++ AutoClicker Interface/MainWindow.xaml.cs
@@ -1,20 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
-
 namespace AutoClicker_Interface
 {
     /// <summary>
@@ -22,8 +8,7 @@ namespace AutoClicker_Interface
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static MouseEvents mouseController=new MouseEvents(); //initalises mouse controller class
-        private Thread clickEventInitaliser;
+        private System.Timers.Timer? clicktime;
         public MainWindow()
         {
             InitializeComponent();
@@ -31,37 +16,62 @@ namespace AutoClicker_Interface
 
         private void startStop_btn_Click(object sender, RoutedEventArgs e)
         {
-            if (int.TryParse(timeDelay_txt.Text, out int i))
+            if (startStop_btn.Content.ToString() == "Start")
             {
-                if (startStop_btn.Content.ToString() == "Start")
+                if (Convert.ToInt32(timeDelay_txt.Text) >= 500)
                 {
-                    startStop_btn.Content = "Stop";
-                    clickEventInitaliser = new Thread(() => runClickEvent(Convert.ToInt32(timeDelay_txt.Text)));
-                    //Application.Current.Dispatcher.Invoke(runClickEvent, DispatcherPriority.ContextIdle);
-                    clickEventInitaliser.Start();
+                    MessageBoxResult result = MessageBox.Show("Are you sure you want to enable the autoclicker with a " + timeDelay_txt.Text + " millisecond delay?", "Enable?", MessageBoxButton.YesNo);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        startStop_btn.Content = "Stop";
+                        clicktime = new(Convert.ToInt32(timeDelay_txt.Text));
+                        if (clickCount_cmb.SelectedItem.ToString() == "Single Click")
+                        {
+                            clicktime.Elapsed += ClickTimerSingle_tick;
+                        }
+                        else 
+                        {
+                            clicktime.Elapsed += ClickTimerDouble_tick; 
+                        }
+                        clicktime.Start();
+                    }
                 }
-                else if (startStop_btn.Content.ToString() == "Stop")
+                else
                 {
-                    startStop_btn.Content = "Start";
+                    MessageBox.Show("To protect the system 500 miliseconds is the minimum delay between clicks. " + timeDelay_txt.Text + " millisecond(s) is too short!", "Time delay too short.", MessageBoxButton.OK);
                 }
             }
             else 
             {
-                MessageBox.Show("The delay entered is not a number.", "Invalid Value!");
+                startStop_btn.Content = "Start";
+                clicktime.Elapsed -= ClickTimerDouble_tick;
+                clicktime.Elapsed -= ClickTimerSingle_tick;
+                clicktime?.Stop();
             }
         }
 
-        private void runClickEvent(int timeDelay)
+        private void ClickTimerSingle_tick(object? sender, System.Timers.ElapsedEventArgs e)
         {
-            string startStopState = "Stop";
-            while (startStopState == "Stop")
-            {
-                //Application.Current.Dispatcher.Invoke(new Action(()=>this.startStop_btn.Content.ToString()), DispatcherPriority.ContextIdle);
-                startStopState= Application.Current.Dispatcher.Invoke<string>(() => { return startStop_btn.Content.ToString(); },DispatcherPriority.Normal);
-                Thread.Sleep(timeDelay);
-                MouseEvents.MouseEvent(MouseEvents.MouseEventFlags.LeftDown);
-                MouseEvents.MouseEvent(MouseEvents.MouseEventFlags.LeftUp);
-            }
+            MouseEvents.MouseEvent(MouseEvents.MouseEventFlags.LeftDown);
+            MouseEvents.MouseEvent(MouseEvents.MouseEventFlags.LeftUp);
+        }
+
+        private void ClickTimerDouble_tick(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            MouseEvents.MouseEvent(MouseEvents.MouseEventFlags.LeftDown);
+            MouseEvents.MouseEvent(MouseEvents.MouseEventFlags.LeftUp);
+            MouseEvents.MouseEvent(MouseEvents.MouseEventFlags.LeftDown);
+            MouseEvents.MouseEvent(MouseEvents.MouseEventFlags.LeftUp);
+        }
+
+        private void timeDelay_txt_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
         }
     }
 }
